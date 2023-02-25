9f2b450a8d32bdb06b3ff18e8946313eb563e35e
mk-comm <113901534+mk-comm@users.noreply.github.com>
2023-02-25 16:30:14 +0100

    test
    test

---
+++ web_crawler/target/debug/deps/web_crawler-4386269ad18a5d36.d
@@ -0,0 +1,7 @@
+c:\Users\mihai\Documents\GitHub\testing\web_crawler\target\debug\deps\web_crawler-4386269ad18a5d36.rmeta: src\main.rs
+
+c:\Users\mihai\Documents\GitHub\testing\web_crawler\target\debug\deps\web_crawler-4386269ad18a5d36.d: src\main.rs
+
+src\main.rs:
+
+# env-dep:CLIPPY_ARGS=
---
+++ web_crawler/target/debug/deps/web_crawler-b1e53cc03637969a.d
@@ -0,0 +1,7 @@
+c:\Users\mihai\Documents\GitHub\testing\web_crawler\target\debug\deps\web_crawler-b1e53cc03637969a.rmeta: src\main.rs
+
+c:\Users\mihai\Documents\GitHub\testing\web_crawler\target\debug\deps\web_crawler-b1e53cc03637969a.d: src\main.rs
+
+src\main.rs:
+
+# env-dep:CLIPPY_ARGS=
